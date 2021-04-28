f29bdef98e83fb7eee735cf593573a29871b4a3d
unknown <rashalismath@gmail.com>
2021-04-28 16:03:28 +0530

    updated

---
+++ src/app/Entities/Enums/PlayerType.ts
@@ -0,0 +1,4 @@
+export enum PlayerType{
+    Captain,
+    Member
+}
--- src/app/Entities/ITeamMember.ts
+++ src/app/Entities/ITeamMember.ts
@@ -1,3 +1,8 @@
+import { PlayerType } from "./Enums/PlayerType";
+import { IUser } from "./IUser";
+
 export interface ITeamMember{
-    
+    id:string;
+    user:IUser;
+    playerType:PlayerType;
 }
